1368135ff7d013046fbfa9dc759593432ba8b6be
Alberto <aaayza@gmail.com>
2022-06-14 20:07:42 -0300

    First 6 completed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,35 +3,48 @@ import React, {useState} from "react";
 export const Calculator = () => {
   const [display, setDisplay] = useState('0');
 
-  const handleClick=e=>{
+  const handleAddKey=e=>{
     console.log(e);
     console.log(e.target.innerHTML);
     const key=e.target.innerHTML;
-    setDisplay(display+key);
+    if(display==='0'){
+      setDisplay(key);
+    }else{
+      const combination=display+key;
+      setDisplay(combination);
+    }
+  };
 
+  const handleResult=e=>{
+    console.log(e.target);
+    const expression=document.getElementById("display").innerHTML;
+    console.log(expression);
+    const result=eval(expression);
+    console.log(result);
+    setDisplay(result);
   };
 
   return (
     <div id="calculator-div">
       <div id="buttons-div">
-      <textarea name="display" id="display" value={display} readOnly />
+      <textarea name="display" id="display" value={display} readOnly/>
         <button id="clear" onClick={(e)=>setDisplay('0')}>AC</button>
-        <button id="divide">/</button>
-        <button id="multiply">*</button>
-        <button id="seven" onClick={handleClick}>7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
-        <button id="subtract">-</button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
-        <button id="add">+</button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-        <button id="equal">=</button>
-        <button id="zero">0</button>
-        <button id="decimal">.</button>
+        <button id="divide" onClick={handleAddKey}>/</button>
+        <button id="multiply" onClick={handleAddKey}>*</button>
+        <button id="seven" onClick={handleAddKey}>7</button>
+        <button id="eight" onClick={handleAddKey}>8</button>
+        <button id="nine" onClick={handleAddKey}>9</button>
+        <button id="subtract" onClick={handleAddKey}>-</button>
+        <button id="four" onClick={handleAddKey}>4</button>
+        <button id="five" onClick={handleAddKey}>5</button>
+        <button id="six" onClick={handleAddKey}>6</button>
+        <button id="add" onClick={handleAddKey}>+</button>
+        <button id="one" onClick={handleAddKey}>1</button>
+        <button id="two" onClick={handleAddKey}>2</button>
+        <button id="three" onClick={handleAddKey}>3</button>
+        <button id="equals" onClick={handleResult}>=</button>
+        <button id="zero" onClick={handleAddKey}>0</button>
+        <button id="decimal" onClick={handleAddKey}>.</button>
       </div>
     </div>
   );
