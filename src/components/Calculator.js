b7899dd2b1de825532393c85f5d8cbdead88573a
Alberto <aaayza@gmail.com>
2022-06-18 20:21:33 -0300

    Reading Regular Expression Operators working

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const Calculator = () => {
-  const [display, setDisplay] = useState('0');
+  const [display, setDisplay] = useState("0");
 
-  const validateOperators=()=>{
-    const regExpRepOps=/('+'|'-'|'*'|'\/'){2,}/;
-    const match=display.match(regExpRepOps);
+  const validateOperators = (expression) => {
+    const regExp=/([\+|\-|\*|\/]){2,}/;
+    const match = expression.match(regExp);
+    console.log(match[0]);   
     return match;
   };
 
-  const handleAddKey=e=>{
+  const handleAddKey = (e) => {
     console.log(e);
     console.log(e.target.innerHTML);
-    const key=e.target.innerHTML;    
-    if(display==='0' && key!=='.'){
+    const key = e.target.innerHTML;
+    if (display === "0" && key !== ".") {
       setDisplay(key);
-    }else{
-      if(display.includes('.') && key!=='.'){
-        let expression=display+key;        
-        setDisplay(expression);        
-      }else{
-        if(!display.includes('.')){
-          let expression2=display+key;        
-          setDisplay(expression2); 
-        }else{
-          if(display.includes('.') && key==='.'){
+    } else {
+      if (display.includes(".") && key !== ".") {
+        let expression = display + key;
+        setDisplay(expression);
+      } else {
+        if (!display.includes(".")) {
+          let expression2 = display + key;
+          setDisplay(expression2);
+        } else {
+          if (display.includes(".") && key === ".") {
             setDisplay(display);
           }
         }
@@ -32,41 +33,75 @@ export const Calculator = () => {
     }
   };
 
-  const handleResult=e=>{
-    if(!validateOperators(display).length){
-        console.log(e.target);
-        const expression=document.getElementById("display").innerHTML;
-        console.log(expression);
-        const result=eval(expression);
-        console.log(result);
-        const strResult=result.toString();
-        setDisplay(strResult);
+  const handleResult = (e) => {
+    console.log(e.target);
+    if (!validateOperators(display)) {
+      const expression = document.getElementById("display").innerHTML;
+      console.log(expression);
+      const result = eval(expression);
+      console.log(result);
+      const strResult = result.toString();
+      setDisplay(strResult);
     }else{
-
+      console.log("Expresion regular mal escrita");
     }
   };
 
   return (
     <div id="calculator-div">
       <div id="buttons-div">
-      <label id="display">{display}</label>
-        <button id="clear" onClick={(e)=>setDisplay('0')}>AC</button>
-        <button id="divide" onClick={handleAddKey}>/</button>
-        <button id="multiply" onClick={handleAddKey}>*</button>
-        <button id="seven" onClick={handleAddKey}>7</button>
-        <button id="eight" onClick={handleAddKey}>8</button>
-        <button id="nine" onClick={handleAddKey}>9</button>
-        <button id="subtract" onClick={handleAddKey}>-</button>
-        <button id="four" onClick={handleAddKey}>4</button>
-        <button id="five" onClick={handleAddKey}>5</button>
-        <button id="six" onClick={handleAddKey}>6</button>
-        <button id="add" onClick={handleAddKey}>+</button>
-        <button id="one" onClick={handleAddKey}>1</button>
-        <button id="two" onClick={handleAddKey}>2</button>
-        <button id="three" onClick={handleAddKey}>3</button>
-        <button id="equals" onClick={handleResult}>=</button>
-        <button id="zero" onClick={handleAddKey}>0</button>
-        <button id="decimal" onClick={handleAddKey}>.</button>
+        <label id="display">{display}</label>
+        <button id="clear" onClick={(e) => setDisplay("0")}>
+          AC
+        </button>
+        <button id="divide" onClick={handleAddKey}>
+          /
+        </button>
+        <button id="multiply" onClick={handleAddKey}>
+          *
+        </button>
+        <button id="seven" onClick={handleAddKey}>
+          7
+        </button>
+        <button id="eight" onClick={handleAddKey}>
+          8
+        </button>
+        <button id="nine" onClick={handleAddKey}>
+          9
+        </button>
+        <button id="subtract" onClick={handleAddKey}>
+          -
+        </button>
+        <button id="four" onClick={handleAddKey}>
+          4
+        </button>
+        <button id="five" onClick={handleAddKey}>
+          5
+        </button>
+        <button id="six" onClick={handleAddKey}>
+          6
+        </button>
+        <button id="add" onClick={handleAddKey}>
+          +
+        </button>
+        <button id="one" onClick={handleAddKey}>
+          1
+        </button>
+        <button id="two" onClick={handleAddKey}>
+          2
+        </button>
+        <button id="three" onClick={handleAddKey}>
+          3
+        </button>
+        <button id="equals" onClick={handleResult}>
+          =
+        </button>
+        <button id="zero" onClick={handleAddKey}>
+          0
+        </button>
+        <button id="decimal" onClick={handleAddKey}>
+          .
+        </button>
       </div>
     </div>
   );
